dd35cc5739295a3b17023004f0bdeb36857d23bf
Amina MANSOUR <amansour@e1r1p12.42.fr>
2019-12-02 19:27:02 +0100

    add softmax function

--- predict.py
+++ predict.py
@@ -6,8 +6,11 @@ from sklearn.metrics import accuracy_score
 def standardize(vector, mean, std):
     return (vector - mean) / std
 def sigmoid (x): return 1/(1 + np.exp(-x))      # activation function
-def sigmoid_(x): return x * (1 - x)             # derivative of sigmoid
+def softmax(X):
+    expX = np.exp(X)
+    return expX / expX.sum(axis=1, keepdims=True)
 
+np.seterr(all = 'ignore')
 f = np.load('weights.npy', allow_pickle=True)
 hidden_layer_1 = f.item().get('hidden_1')
 hidden_layer_2 = f.item().get('hidden_2')
@@ -15,10 +18,12 @@ output_layer = f.item().get('output')
 mean = f.item().get('mean')
 std = f.item().get('std')
 df = pd.read_csv('test.csv')
-Y = df.column_1
+Y = df['column_1'].map({'M': 1, 'B': 0}).values
+df = df.drop('column_1', axis=1)
 for i in range(32):
-    df['column_' + str(i)] = standardize(df['column_' + str(i)], mean[i], std[i])
-X = df.drop('column_1', axis=1).values
+    if i != 1:
+        df['column_' + str(i)] = standardize(df['column_' + str(i)], mean[i], std[i])
+X = df.values
 add = np.ones((X.shape[0], 1))
 X = np.concatenate((X, add), axis=1)
 ### hidden layer 1 ###
@@ -28,13 +33,13 @@ hidden_outputs_1 = sigmoid(z_h_1)
 z_h_2 = np.dot(hidden_outputs_1, hidden_layer_2)
 hidden_outputs_2 = sigmoid(z_h_2)
 ### output layer ###
-z_o = np.dot(hidden_outputs_2, output_layer)
-final_outputs = sigmoid(z_o)
+z_o = np.dot(hidden_outputs_2, output_layer.T)
+final_outputs = softmax(z_o)
 Y_predict = []
 for x in final_outputs:
-    if x > 0.5:
+    if x[0] > x[1]:
         Y_predict.append(1)
     else:
         Y_predict.append(0)
-
+Y_predict = np.array(Y_predict)
 print(accuracy_score(Y, Y_predict))
--- train.py
+++ train.py
@@ -6,17 +6,31 @@ import numpy as np
 from math import *
 from sklearn.metrics import accuracy_score
 
-def sigmoid (x): return 1/(1 + np.exp(-x))      # activation function
-def sigmoid_(x): return x * (1 - x)             # derivative of sigmoid
-def softmax(x): return np.exp(x) / sum(np.exp(x))
+def cross_entropy(p,y):
+    m = y.shape[0]
+    print(p)
+    log_likelihood = -np.log(p[range(m),y])
+    loss = np.sum(log_likelihood) / m
+    return loss
+
+def delta_cross_entropy(X,y):
+    m = y.shape[0]
+    grad = softmax(X)
+    grad[range(m),y] -= 1
+    grad = grad/m
+    return grad
+
+def sigmoid (x): return (1/(1 + np.exp(-x)))      # activation function
+def sigmoid_(x): return (x * (1 - x))             # derivative of sigmoid
+def softmax(X):
+    expX = np.exp(X)
+    return expX / expX.sum(axis=1, keepdims=True)
+
 def scale_data(df):
     return (df - df.mean()) / df.std()
 
 class Neuron(object): 
-    bias = 0 
-    output = 0 
-    weights = [] 
-    def __init__(self, prev_layer_size): 
+    def __init__(self, prev_layer_size):
         self.weights = [random() for i in range(prev_layer_size)]
     def __repr__(self): 
         return '{"bias":"' + str(self.bias) + '" , "output":"' + str(self.output) + '" , "weights":' + str(self.weights) +  '}' 
@@ -57,17 +71,25 @@ for i in range(32):
     columns.append('column_' + str(i))
 df = pd.read_csv('data_training.csv', names=columns)
 # numerize column_1
-df['column_1'] = df['column_1'].map({'M': 1, 'B': 0})
+df['M'] = df['column_1'].map({'M': 1, 'B': 0})
+df['B'] = df['column_1'].map({'M': 0, 'B': 1})
 # split dataset
-target = df.column_1
+target = df['M']
 # create training and testing vars
 X_train_0, X_test, y_train, y_test = train_test_split(df, target, test_size=0.2)
-X_train = X_train_0.drop('column_1', axis=1)
+Y_init = X_train_0.M
+X_train_0 = X_train_0.drop('column_1', axis=1)
+X_train = X_train_0.drop('M', axis=1)
+X_test = X_test.drop('B', axis=1)
+X_test = X_test.drop('M', axis=1)
 X_test.to_csv('test.csv', index=False)
+Y_M = np.reshape(y_train.values, (X_train.shape[0], 1))
+Y_B = np.reshape(X_train.B.values, (X_train.shape[0], 1))
+X_train = X_train.drop('B', axis=1)
 X_train_scale = scale_data(X_train)
 # Network
 epochs = 1000    # Number of iterations
-inputLayerSize, hiddenLayerSize, outputLayerSize = X_train.shape[1] + 1, X_train.shape[1] + 1, 1
+inputLayerSize, hiddenLayerSize, outputLayerSize = X_train.shape[1] + 1, X_train.shape[1] + 1, 2
 L = 0.03    # learning rate
 network = Network()
 network.add_layer(inputLayerSize, 'input')
@@ -79,7 +101,7 @@ network.add_layer(outputLayerSize, 'output')
 X = np.reshape(X_train_scale.values, (X_train.shape[0], X_train.shape[1]))
 add = np.ones((X_train.shape[0], 1))
 X = np.concatenate((X, add), axis=1)
-Y = np.reshape(y_train.values, (X_train.shape[0], 1))
+Y = np.concatenate((Y_M, Y_B), axis=1)
 for index, neuron in enumerate(network.layers[0].neurons):
     neuron.output = X[index]
 network.layers[0].add_outputs()
@@ -87,51 +109,49 @@ network.layers[1].add_weights()
 network.layers[2].add_weights()
 network.layers[3].add_weights()
 ### outlayer ###
-network.layers[3].weights = np.reshape(network.layers[3].weights, (X_train.shape[1] + 1, 1))
-
+#network.layers[3].weights = np.reshape(network.layers[3].weights, (X_train.shape[1] + 1, outputLayerSize))
 for i in range(epochs):
-    # forward propagation
+    ############# feedforward
     ### Hidden layer 1 ###
     z_h_1 = np.dot(X, network.layers[1].weights)
     network.layers[1].outputs = sigmoid(z_h_1)
     ### Hidden layer 2 ###
-    z_h_2 = np.dot(X, network.layers[2].weights)
+    z_h_2 = np.dot(network.layers[1].outputs, network.layers[2].weights)
     network.layers[2].outputs = sigmoid(z_h_2)
     ###output###
-    z_o = np.dot(network.layers[2].outputs, network.layers[3].weights)
-    network.layers[3].outputs = sigmoid(z_o)
-    # Calculate the error
-    E = Y - network.layers[3].outputs                                           # how much we missed (error)
-    err = ((1 / 2) * (np.power((E), 2)))
-    # Backpropagation
+    z_o = np.dot(network.layers[2].outputs, network.layers[3].weights.T)
+    network.layers[3].outputs = softmax(z_o)
+    ### Backpropagation
     ## Output layer
-    delta_z_o = sigmoid_(network.layers[3].outputs)
+    delta_z_o = Y - network.layers[3].outputs
     delta_w13 = network.layers[2].outputs
-    delta_output_layer = np.dot(delta_w13.T,(E * delta_z_o))
+    delta_output_layer = np.dot(delta_z_o.T, delta_w13)
     ## Hidden layer 2
-    delta_a_h_2 = np.dot(E * delta_z_o, network.layers[3].weights.T)
+    delta_a_h_2 = np.dot(delta_z_o, network.layers[3].weights)
     delta_z_h_2 = sigmoid_(network.layers[2].outputs)
     delta_w12 = network.layers[1].outputs
     delta_hidden_layer_2 = np.dot(delta_w12.T, delta_a_h_2 * delta_z_h_2)
     ## Hidden layer 1
-    delta_a_h_1 = np.dot(E * delta_z_h_2, network.layers[2].weights.T)
+    delta_a_h_1 = np.dot(delta_z_h_2, network.layers[2].weights)
     delta_z_h_1 = sigmoid_(network.layers[1].outputs)
     delta_w11 = X
     delta_hidden_layer_1 = np.dot(delta_w11.T, delta_a_h_1 * delta_z_h_1)
-
     network.layers[1].weights = network.layers[1].weights + L * delta_hidden_layer_1
     network.layers[2].weights = network.layers[2].weights + L * delta_hidden_layer_2
-    network.layers[3].weights =  network.layers[3].weights + L * delta_output_layer
-    print("epoch {}/{} - loss: {} - val_loss: {}".format(i + 1, epochs, err.sum(), 0))
+    network.layers[3].weights = network.layers[3].weights + L * delta_output_layer
+    #### Loss function
+    loss = np.sum(Y * np.log(network.layers[3].outputs)) / X.shape[0]
+    print("epoch {}/{} - loss: {} - val_loss: {}".format(i + 1, epochs,loss, 0))
 
 Y_predict = []
 # print(list(network.layers[3].outputs))
 for x in network.layers[3].outputs:
-    if x >= 0.5:
+    if x[0] > x[1]:
         Y_predict.append(1)
     else:
         Y_predict.append(0)
 Y_predict = np.array(Y_predict)
+print(accuracy_score(Y_init, Y_predict))
 weights_dic = {}
 weights_dic['hidden_1'] = network.layers[1].weights
 weights_dic['hidden_2'] = network.layers[2].weights
