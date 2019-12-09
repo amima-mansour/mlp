4d4b64364898da47bf3b26aaa204a1d24dbfaa04
Amina MANSOUR <amansour@e1r2p16.42.fr>
2019-12-09 11:34:11 +0100

    parameters set

--- predict.py
+++ predict.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from tools import predict, sigmoid
 from math import log
+import sys
+import argparse
 
 def standardize(vector, mean, std):
     return (vector - mean) / std
@@ -12,33 +14,38 @@ def softmax(X):
     expX = np.exp(X)
     return expX / expX.sum(axis=1, keepdims=True)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", help="Set Dataset to predict")
+parser.add_argument("--weights", help="Set weights file")
+args = parser.parse_args()
 np.seterr(all = 'ignore')
-f = np.load('weights.npy', allow_pickle=True)
-weights_hidden_1 = f.item().get('hidden_1_w')
-weights_hidden_2 = f.item().get('hidden_2_w')
-weights_outputs = f.item().get('output_w')
-bias_hidden_1 = f.item().get('hidden_1_b')
-bias_hidden_2 = f.item().get('hidden_2_b')
-bias_output = f.item().get('output_b')
-mean = f.item().get('mean')
-std = f.item().get('std')
-df = pd.read_csv('test.csv')
-j = 0
-for i in range(30):
-    if i != 1:
-        df['column_' + str(i)] = standardize(df['column_' + str(i)], mean[j], std[j])
-        j += 1
-
-Y_M = df['column_1'].map({'M': 1, 'B': 0}).values
-Y_B = df['column_1'].map({'M': 0, 'B': 1}).values
-Y_M = np.reshape(Y_M, (Y_B.shape[0], 1))
-Y_B = np.reshape(Y_B, (Y_B.shape[0], 1))
-Y = np.concatenate((Y_M, Y_B), axis=1)
-df = df.drop('column_1', axis=1)
-X = df.values
-Y_predict = predict(X, weights_hidden_1, bias_hidden_1, weights_hidden_2, bias_hidden_2, weights_outputs, bias_output)
-print('Accuracy test = {:.2f}'.format(accuracy_score(Y[:, 0], Y_predict)))
-### Cross entropy
-cross = -np.sum(Y[:, 0] * np.log(Y_predict.T + 1e-9) + (1 - Y[:, 0]) * np.log(1 - Y_predict.T + 1e-9)) / Y_predict.shape[0]
-# cross = crossEntropyLoss(Y_predict, Y[:, 0])
-print('Cross Entropy value = {:.5f}'.format(cross))
+try:
+    f = np.load(args.weights, allow_pickle=True)
+    weights_hidden_1 = f.item().get('hidden_1_w')
+    weights_hidden_2 = f.item().get('hidden_2_w')
+    weights_outputs = f.item().get('output_w')
+    bias_hidden_1 = f.item().get('hidden_1_b')
+    bias_hidden_2 = f.item().get('hidden_2_b')
+    bias_output = f.item().get('output_b')
+    mean = f.item().get('mean')
+    std = f.item().get('std')
+    df = pd.read_csv(args.dataset)
+    j = 0
+    for i in range(30):
+        if i != 1:
+            df['column_' + str(i)] = standardize(df['column_' + str(i)], mean[j], std[j])
+            j += 1
+    Y_M = df['column_1'].map({'M': 1, 'B': 0}).values
+    Y_B = df['column_1'].map({'M': 0, 'B': 1}).values
+    Y_M = np.reshape(Y_M, (Y_B.shape[0], 1))
+    Y_B = np.reshape(Y_B, (Y_B.shape[0], 1))
+    Y = np.concatenate((Y_M, Y_B), axis=1)
+    df = df.drop('column_1', axis=1)
+    X = df.values
+    Y_predict = predict(X, weights_hidden_1, bias_hidden_1, weights_hidden_2, bias_hidden_2, weights_outputs, bias_output)
+    print('Accuracy test = {:.2f}'.format(accuracy_score(Y[:, 0], Y_predict)))
+    ### Cross entropy
+    cross = -np.sum(Y[:, 0] * np.log(Y_predict.T + 1e-9) + (1 - Y[:, 0]) * np.log(1 - Y_predict.T + 1e-9)) / Y_predict.shape[0]
+    print('Cross Entropy value = {:.5f}'.format(cross))
+except:
+    print("Error dataset to predict or weights")
--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 from  matplotlib import pyplot as plt
 from tools import predict, final_outputs, sigmoid, softmax
+import argparse
 
 def standardize(vector, mean, std):
     return (vector - mean) / std
@@ -63,125 +64,127 @@ class Network(object):
         return '{"Network":{"layers":' + str(self.layers) + '}}'
 
 np.seterr(all = 'ignore')
-# read dataset
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", help="Set Dataset name to train")
+args = parser.parse_args()
+#dataset columns
 columns = []
 for i in range(32):
     columns.append('column_' + str(i))
-df = pd.read_csv('data.csv', names=columns)
-# numerize column_1
-df['M'] = df['column_1'].map({'M': 1, 'B': 0})
-df['B'] = df['column_1'].map({'M': 0, 'B': 1})
-# split dataset
-target = df['M']
-
-#splitting data
-X_train_0, X_test, y_train, y_test = train_test_split(df, target, test_size=0.2, random_state=1)
-X_train_0, X_val, y_train, y_val = train_test_split(X_train_0, y_train, test_size=0.2, random_state=1)
-## test data
-X_test = X_test.drop('B', axis=1)
-X_test = X_test.drop('M', axis=1)
-X_test.to_csv('test.csv', index=False)
-## train data
-X_train = X_train_0.drop(['column_1','M'], axis=1)
-Y_M = np.reshape(y_train.values, (X_train.shape[0], 1))
-Y_B = np.reshape(X_train.B.values, (X_train.shape[0], 1))
-X_train = X_train.drop('B', axis=1)
-mean, std = X_train.mean().tolist(), X_train.std().tolist()
-X_train_scale = scale_data(X_train)
-X_train = np.reshape(X_train_scale.values, (X_train.shape[0], X_train.shape[1]))
-Y_train = np.concatenate((Y_M, Y_B), axis=1)
-#### Validation data
-X_val = X_val.drop(['column_1', 'M'], axis=1)
-Y_M = np.reshape(y_val.values, (X_val.shape[0], 1))
-Y_B = np.reshape(X_val.B.values, (X_val.shape[0], 1))
-X_val = X_val.drop('B', axis=1)
-X_val_scale = standardize(X_val, mean, std)
-X_val = np.reshape(X_val_scale.values, (X_val.shape[0], X_val.shape[1]))
-Y_val = np.concatenate((Y_M, Y_B), axis=1)
-#### Shuffle
-X, Y = shuffle(X_train, Y_train, random_state=np.random.RandomState())
-X_val, Y_val = shuffle(X_val, Y_val, random_state=np.random.RandomState())
-# Network
-epochs = 250    # Number of iterations
-inputLayerSize, hiddenLayerSize_1, hiddenLayerSize_2, outputLayerSize = X_train.shape[1], 16, 8,2
-L = 0.3  # learning rate
-network = Network()
-network.add_layer(inputLayerSize, 'input')
-network.add_layer(hiddenLayerSize_1, 'hidden_1')
-network.add_layer(hiddenLayerSize_2, 'hidden_2')
-network.add_layer(outputLayerSize, 'output')
-for index, neuron in enumerate(network.layers[0].neurons):
-    neuron.output = X[index]
-network.layers[0].add_outputs()
-network.layers[1].add_weights()
-network.layers[2].add_weights()
-network.layers[3].add_weights()
-network.layers[1].add_bias()
-network.layers[2].add_bias()
-network.layers[3].add_bias()
-val_loss, loss = [], []
-for i in range(epochs):
-    ############# feedforward
-    ### Hidden layer 1 ###
-    z_h_1 = X.dot(network.layers[1].weights.T) + network.layers[1].bias
-    network.layers[1].outputs = sigmoid(z_h_1)
-    ### Hidden layer 2 ###
-    z_h_2 = network.layers[1].outputs.dot(network.layers[2].weights.T) + network.layers[2].bias
-    network.layers[2].outputs = sigmoid(z_h_2)
-    ###output###
-    z_o = np.dot(network.layers[2].outputs, network.layers[3].weights.T) + network.layers[3].bias
-    network.layers[3].outputs = softmax(z_o)
-    ### Backpropagation
-    ## Output layer
-    delta_z_o = network.layers[3].outputs - Y
-    delta_w13 = network.layers[2].outputs
-    dw_o = np.dot(delta_z_o.T, delta_w13) / X.shape[0]
-    db_o = np.sum(delta_z_o, axis=0, keepdims=True) / X.shape[0]
-    ## Hidden layer 2
-    delta_a_h_2 = np.dot(delta_z_o, network.layers[3].weights)
-    delta_z_h_2 = sigmoid_(network.layers[2].outputs)
-    d = delta_a_h_2 * delta_z_h_2
-    delta_w12 = network.layers[1].outputs
-    dw_h_2 = np.dot(d.T, delta_w12) / X.shape[0]
-    db_h_2 = np.sum(d, axis=0, keepdims=True) / X.shape[0]
-    ## Hidden layer 1
-    delta_a_h_1 = delta_z_h_2.dot(network.layers[2].weights)
-    delta_z_h_1 = sigmoid_(network.layers[1].outputs)
-    d =  delta_a_h_1 * delta_z_h_1
-    delta_w11 = X
-    dw_h_1 = np.dot(d.T, delta_w11) / X.shape[0]
-    db_h_1 = np.sum(d, axis=0, keepdims=True) / X.shape[0]
-    # print(db_h_1)
-    ### Update weights and bias
-    network.layers[1].weights -= L * dw_h_1
-    network.layers[2].weights -= L * dw_h_2
-    network.layers[3].weights -= L * dw_o
-    network.layers[1].bias -= L * db_h_1
-    network.layers[2].bias -= L * db_h_2
-    network.layers[3].bias -= L * db_o
-    #### Loss function
-    Y_predict = final_outputs(network.layers[3].outputs)
-    loss.append(mean_square_error(Y[:, 0], Y_predict))
-    Y_val_predict = predict(X_val, network.layers[1].weights, network.layers[1].bias, network.layers[2].weights, network.layers[2].bias, network.layers[3].weights, network.layers[3].bias)
-    val_loss.append(mean_square_error(Y_val[:, 0], Y_val_predict))
-    print("epoch {}/{} - loss: {:.10f} - val_loss: {:.10f}".format(i + 1, epochs, loss[i], val_loss[i]))
-
-plt.plot(range(1, epochs + 1), loss, 'g--', label='loss')
-plt.plot(range(1, epochs + 1), val_loss, 'r--', label='val_loss')
-plt.xlabel('epochs')
-plt.ylabel('loss value')
-plt.legend()
-plt.show()
-
-print('Accuracy training = {:.2f}'.format(accuracy_score(Y[:, 0], Y_predict)))
-print('Accuracy validation = {:.2f}'.format(accuracy_score(Y_val[:, 0], Y_val_predict)))
-weights_dic = {}
-weights_dic['hidden_1_w'] = network.layers[1].weights
-weights_dic['hidden_2_w'] = network.layers[2].weights
-weights_dic['output_w'] = network.layers[3].weights
-weights_dic['hidden_1_b'] = network.layers[1].bias
-weights_dic['hidden_2_b'] = network.layers[2].bias
-weights_dic['output_b'] = network.layers[3].bias
-weights_dic['mean'] = mean
-weights_dic['std'] = std
-np.save('weights.npy', weights_dic)
+try:
+    df = pd.read_csv(args.dataset, names=columns)
+    # numerize column_1
+    df['M'] = df['column_1'].map({'M': 1, 'B': 0})
+    df['B'] = df['column_1'].map({'M': 0, 'B': 1})
+    # split dataset
+    target = df['M']
+    X_train_0, X_test, y_train, y_test = train_test_split(df, target, test_size=0.2, random_state=1)
+    X_train_0, X_val, y_train, y_val = train_test_split(X_train_0, y_train, test_size=0.2, random_state=1)
+    ## test data
+    X_test = X_test.drop('B', axis=1)
+    X_test = X_test.drop('M', axis=1)
+    X_test.to_csv('test.csv', index=False)
+    ## train data
+    X_train = X_train_0.drop(['column_1','M'], axis=1)
+    Y_M = np.reshape(y_train.values, (X_train.shape[0], 1))
+    Y_B = np.reshape(X_train.B.values, (X_train.shape[0], 1))
+    X_train = X_train.drop('B', axis=1)
+    mean, std = X_train.mean().tolist(), X_train.std().tolist()
+    X_train_scale = scale_data(X_train)
+    X_train = np.reshape(X_train_scale.values, (X_train.shape[0], X_train.shape[1]))
+    Y_train = np.concatenate((Y_M, Y_B), axis=1)
+    #### Validation data
+    X_val = X_val.drop(['column_1', 'M'], axis=1)
+    Y_M = np.reshape(y_val.values, (X_val.shape[0], 1))
+    Y_B = np.reshape(X_val.B.values, (X_val.shape[0], 1))
+    X_val = X_val.drop('B', axis=1)
+    X_val_scale = standardize(X_val, mean, std)
+    X_val = np.reshape(X_val_scale.values, (X_val.shape[0], X_val.shape[1]))
+    Y_val = np.concatenate((Y_M, Y_B), axis=1)
+    #### Shuffle
+    X, Y = shuffle(X_train, Y_train, random_state=np.random.RandomState())
+    X_val, Y_val = shuffle(X_val, Y_val, random_state=np.random.RandomState())
+    # Network
+    epochs = 250    # Number of iterations
+    inputLayerSize, hiddenLayerSize_1, hiddenLayerSize_2, outputLayerSize = X_train.shape[1], 16, 8,2
+    L = 0.3  # learning rate
+    network = Network()
+    network.add_layer(inputLayerSize, 'input')
+    network.add_layer(hiddenLayerSize_1, 'hidden_1')
+    network.add_layer(hiddenLayerSize_2, 'hidden_2')
+    network.add_layer(outputLayerSize, 'output')
+    for index, neuron in enumerate(network.layers[0].neurons):
+        neuron.output = X[index]
+    network.layers[0].add_outputs()
+    network.layers[1].add_weights()
+    network.layers[2].add_weights()
+    network.layers[3].add_weights()
+    network.layers[1].add_bias()
+    network.layers[2].add_bias()
+    network.layers[3].add_bias()
+    val_loss, loss = [], []
+    for i in range(epochs):
+        ############# feedforward
+        ### Hidden layer 1 ###
+        z_h_1 = X.dot(network.layers[1].weights.T) + network.layers[1].bias
+        network.layers[1].outputs = sigmoid(z_h_1)
+        ### Hidden layer 2 ###
+        z_h_2 = network.layers[1].outputs.dot(network.layers[2].weights.T) + network.layers[2].bias
+        network.layers[2].outputs = sigmoid(z_h_2)
+        ###output###
+        z_o = np.dot(network.layers[2].outputs, network.layers[3].weights.T) + network.layers[3].bias
+        network.layers[3].outputs = softmax(z_o)
+        ### Backpropagation
+        ## Output layer
+        delta_z_o = network.layers[3].outputs - Y
+        delta_w13 = network.layers[2].outputs
+        dw_o = np.dot(delta_z_o.T, delta_w13) / X.shape[0]
+        db_o = np.sum(delta_z_o, axis=0, keepdims=True) / X.shape[0]
+        ## Hidden layer 2
+        delta_a_h_2 = np.dot(delta_z_o, network.layers[3].weights)
+        delta_z_h_2 = sigmoid_(network.layers[2].outputs)
+        d = delta_a_h_2 * delta_z_h_2
+        delta_w12 = network.layers[1].outputs
+        dw_h_2 = np.dot(d.T, delta_w12) / X.shape[0]
+        db_h_2 = np.sum(d, axis=0, keepdims=True) / X.shape[0]
+        ## Hidden layer 1
+        delta_a_h_1 = delta_z_h_2.dot(network.layers[2].weights)
+        delta_z_h_1 = sigmoid_(network.layers[1].outputs)
+        d = delta_a_h_1 * delta_z_h_1
+        delta_w11 = X
+        dw_h_1 = np.dot(d.T, delta_w11) / X.shape[0]
+        db_h_1 = np.sum(d, axis=0, keepdims=True) / X.shape[0]
+        ### Update weights and bias
+        network.layers[1].weights -= L * dw_h_1
+        network.layers[2].weights -= L * dw_h_2
+        network.layers[3].weights -= L * dw_o
+        network.layers[1].bias -= L * db_h_1
+        network.layers[2].bias -= L * db_h_2
+        network.layers[3].bias -= L * db_o
+        #### Loss function
+        Y_predict = final_outputs(network.layers[3].outputs)
+        loss.append(mean_square_error(Y[:, 0], Y_predict))
+        Y_val_predict = predict(X_val, network.layers[1].weights, network.layers[1].bias, network.layers[2].weights,
+            network.layers[2].bias, network.layers[3].weights, network.layers[3].bias)
+        val_loss.append(mean_square_error(Y_val[:, 0], Y_val_predict))
+        print("epoch {}/{} - loss: {:.10f} - val_loss: {:.10f}".format(i + 1, epochs, loss[i], val_loss[i]))
+    plt.plot(range(1, epochs + 1), loss, 'g--', label='loss')
+    plt.plot(range(1, epochs + 1), val_loss, 'r--', label='val_loss')
+    plt.xlabel('epochs')
+    plt.ylabel('loss value')
+    plt.legend()
+    plt.show()
+    print('Accuracy training = {:.2f}'.format(accuracy_score(Y[:, 0], Y_predict)))
+    print('Accuracy validation = {:.2f}'.format(accuracy_score(Y_val[:, 0], Y_val_predict)))
+    weights_dic = {}
+    weights_dic['hidden_1_w'] = network.layers[1].weights
+    weights_dic['hidden_2_w'] = network.layers[2].weights
+    weights_dic['output_w'] = network.layers[3].weights
+    weights_dic['hidden_1_b'] = network.layers[1].bias
+    weights_dic['hidden_2_b'] = network.layers[2].bias
+    weights_dic['output_b'] = network.layers[3].bias
+    weights_dic['mean'] = mean
+    weights_dic['std'] = std
+    np.save('weights.npy', weights_dic)
+except:
+    print("Error dataset")
